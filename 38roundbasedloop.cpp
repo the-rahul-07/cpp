4df99a07f331e233f2df78cad16f6beb40cd5ea8
Rahul <rahulgupta2019007@gmail.com>
2026-01-09 00:11:36 +0530

    round based for loop

---
+++ 38roundbasedloop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std ::cin;
+using std ::cout;
+
+int main()
+{
+    int data[] = {2, 5, 7, 8};
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << data[i] << "\t";
+    }
+    cout << "\n";
+    for (int n : data)
+    {
+        cout << n << "\t";
+    }
+
+    return 0;
+}
